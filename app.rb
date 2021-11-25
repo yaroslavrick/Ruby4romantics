44981997ce95194e86507ecd175c2ef4980aad89
Yaroslav Yenkala <yaroslavrick@gmail.com>
2021-11-25 23:13:36 +0200

    per 5 years

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 puts "Your salary a month"
 salary = gets.to_i
 defer_paycheck = 0.15
-puts "Your salary per year is #{salary * 12} $"
-puts "You'll defer paycher if it's 15% #{salary * 12 * defer_paycheck} per year"
+puts "=" * 80
+puts "Your salary per year is #{salary * 12} uah"
+puts "You'll defer paycher if it's 15% #{(salary * 12 * defer_paycheck).to_i} uah per year"
+puts "You'll defer paycher (if it's 15%) #{((salary * 12 * defer_paycheck)*5).to_i} uah per 5 year"
