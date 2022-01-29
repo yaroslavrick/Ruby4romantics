10e986d75c7493be4f1c73c2263e4b196fba1f0e
Yaroslav <yaroslavrick@gmail.com>
2022-01-29 15:01:39 +0200

    renamed 1 to 01 and so on

--- 15_OOP-1.rb
+++ 15_OOP-1.rb
@@ -86,7 +86,7 @@ puts "Files: #{Computer.display_files}"
 #                   Counting persons
 
 class Person
-  @@people_count = 0
+  @@people_count = 0 # declaring class variable @@ 
   
   def initialize(name)
     @name = name
@@ -94,7 +94,7 @@ class Person
   end
   
   def self.number_of_instances
-    @@people_count# Return your class variable
+    @@people_count# Return your class variable. Место для хранения переменной класса
   end
 end
 
@@ -103,3 +103,96 @@ dhh = Person.new("David")
 
 puts "Number of Person instances: #{Person.number_of_instances}"
 # Output: Number of Person instances: 2
+
+#####################
+# Inheritance   Наследование
+
+=begin
+Inheritance is the process by which one class takes on the attributes and methods 
+of another, and it’s used to express an is-a relationship. For example, a cartoon 
+fox is a cartoon mammal, so a CartoonFox class could inherit from a CartoonMammal class. 
+=end
+
+class ApplicationError
+  def display_error
+    puts "Error! Error!"
+  end
+end
+
+class SuperBadError < ApplicationError # Inheritance. You can read “<“ as “inherits from.
+end
+
+err = SuperBadError.new
+err.display_error
+ 
+=begin
+Check out the code in the editor. We’ve defined a class, ApplicationError, as 
+well as a SuperBadError class that inherits from ApplicationError. Note that we 
+don’t define the display_error method in the body of SuperBadError, but it will still 
+have access to that method via inheritance.
+=end
+
+class Creature
+  def initialize(name)
+    @name = name
+  end
+  
+  def fight
+    return "Punch to the chops!"
+  end
+end
+
+class Dragon < Creature
+  def fight      # Give your derived class a fight method that overrides Creature‘s; instead of returning “Punch to the chops!”, it should return “Breathes fire!”.               
+    return "Breathes fire!"
+  end
+end
+################################################
+
+
+class Machine
+  @@users = {}
+  
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @@users[username] = password # so that your @@users hash keeps usernames as keys with each username’s password as the associated value.
+    @files = {} # instance variable and set this to an empty hash. This is so we can keep track of our files later!
+  end
+  
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created by #{@username} at #{time}."
+  end
+  
+  def Machine.get_users
+    @@users
+  end
+end
+
+my_machine = Machine.new("eric", 01234) # we create a new instance of Machine
+your_machine = Machine.new("you", 56789)
+
+my_machine.create("groceries.txt")
+your_machine.create("todo.txt")
+
+puts "Users: #{Machine.get_users}"
+
+=begin
+Who are the Users?
+
+Perfect! Now that we think of it, though, we could have a whole bunch of users creating
+files every which where, and we don’t have a way of getting to our @@users class variable! 
+We’ll need to create a method to reach it.
+
+Because @@users is a class variable, we’ll use a class method to grab it. 
+Most of the methods you’ve seen so far are instance methods—they work on a 
+particular instance/object, such as “matz” or “blixy”. A class method belongs 
+to the class itself, and for that reason, it’s prefixed with the class name.
+=end
+class Machine
+  def Machine.hello
+    puts "Hello from the machine!"
+  end
+end
