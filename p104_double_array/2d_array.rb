ba483f9ca55d4e736c7841d494e886a6396c3b50
Yaroslav Y <yaroslavrick@gmail.com>
2022-07-18 17:42:15 +0300

    double arrays study

--- p104_double_array/2d_array.rb
+++ p104_double_array/2d_array.rb
@@ -4,3 +4,14 @@ arr = Array.new(10) { Array.new(10) }
 # Создать массив в 2 строки и 3 столбца и заполнить каждую строку одинаковым объектом
 # “something”:
 arr = Array.new(2) { Array.new(3, 'something') }
+
+# Крестики-нолики:
+arr = [
+  [0, 0, 1],
+  [nil, 0, nil],
+  [1, nil, 1]
+]
+
+puts arr.to_s
+# [[0, 0, 1], [nil, 0, nil], [1, nil, 1]]
+
