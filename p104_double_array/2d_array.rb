3eb77451daa4136cc333ef59ca5e68190dafcc26
Yaroslav Y <yaroslavrick@gmail.com>
2022-07-18 16:17:02 +0300

    studying 2d arrays in ruby

---
+++ p104_double_array/2d_array.rb
@@ -0,0 +1,6 @@
+# Makind 2d array (10x10):
+arr = Array.new(10) { Array.new(10) }
+
+# Создать массив в 2 строки и 3 столбца и заполнить каждую строку одинаковым объектом
+# “something”:
+arr = Array.new(2) { Array.new(3, 'something') }
